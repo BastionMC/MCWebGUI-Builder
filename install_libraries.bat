553515f97739bf36d26455f325271b1f0288e9e9
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2023-10-07 16:08:31 +0200

    animated images lesss gooo aaa

--- install_libraries.bat
+++ install_libraries.bat
@@ -3,6 +3,4 @@ echo Installing "xmltodict" from PyPI
 pip install xmltodict
 echo Installing "colorama" from PyPI
 pip install colorama
-echo Installing "xmltodict" from PyPI
-pip install Pillow
 echo Done!
